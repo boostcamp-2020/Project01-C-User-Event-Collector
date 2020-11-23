8da3d3ab401acbbf699bb57d587ef8982a399948
nrurnru <nrurnru@gmail.com>
2020-11-23 18:51:40 +0900

    [iOS] Feat : Now. 다시듣기 뷰 구현
    더미 데이터 사용해서 표시 확인
    
    #20

--- iOS/MiniVIBE/MiniVIBE/NowSectionView.swift
+++ iOS/MiniVIBE/MiniVIBE/NowSectionView.swift
@@ -8,47 +8,70 @@
 import SwiftUI
 
 struct NowSectionView: View {
+    @State var songItems: [SongItem]
+    
     var body: some View {
-        ZStack {
-            Color.black
-            
-            VStack(alignment: .leading ) {
-                HStack {
-                    Image("home-now")
-                        .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100, height: 30, alignment: .center)
-                        .foregroundColor(.pink)
-                    Text("다시듣기").font(.title)
-                }.padding(.leading)
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Image("now-dummy1")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 230, height: 300, alignment: .leading).aspectRatio(contentMode: .fit)
-                            Text("야간작업실")
-                        }
-                        VStack(alignment: .leading) {
-                            Image("now-dummy2")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 230, height: 300, alignment: .leading).aspectRatio(contentMode: .fit)
-                            Text("어벤걸스")
-                        }
-                    }
-                    .padding(.leading)
-                }.statusBar(hidden: true)
-            }
+        VStack(alignment: .leading) {
+            HStack {
+                Image("home-now")
+                    .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 30, alignment: .leading)
+                    .foregroundColor(.white)
+                Text("다시듣기").font(.system(size: 30)).foregroundColor(.white)
+            }.padding(.leading)
+            NowSectionScrollView(songItems: $songItems)
         }
-        
     }
+}
+
+struct NowSectionScrollView: View {
+    @Binding var songItems: [SongItem]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 20.0) {
+                ForEach(songItems, id: \.id) { item in
+                    NowSectionItemView(item: item)
+                }
+            }
+            .padding(.horizontal, 10)
+        }.statusBar(hidden: true)
+        .onAppear { UIScrollView.appearance().isPagingEnabled = true }
+    }
+}
+
+struct NowSectionItemView: View {
+    let imageWidth = (UIScreen.main.bounds.width - 40) / 2
+    var item: SongItem
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            item.albumArt
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: imageWidth, height: 250, alignment: .center)
+                .clipped()
+            Text(item.description).foregroundColor(.white)
+        }
+    }
+}
+
+struct SongItem: Identifiable {
+    var id = UUID()
+    
+    var albumArt: Image
+    var description: String
 }
 
 struct NowSectionView_Previews: PreviewProvider {
     static var previews: some View {
-        NowSectionView()
+        NowSectionView(songItems: [
+            SongItem(albumArt: Image("now-dummy1"), description: "야간작업실"),
+            SongItem(albumArt: Image("now-dummy2"), description: "어벤걸스"),
+            SongItem(albumArt: Image("now-dummy1"), description: "야간작업실"),
+            SongItem(albumArt: Image("now-dummy2"), description: "어벤걸스")
+        ]).background(Color.black)
     }
 }
