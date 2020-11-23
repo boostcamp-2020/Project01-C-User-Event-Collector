4b4a84998f9b0af09f3192a5e53d4ce383fccf86
nrurnru <nrurnru@gmail.com>
2020-11-23 15:44:00 +0900

    [iOS] Feat: Home Footer 내부 뷰 추상화
    텍스트뷰, 디바이더 뷰 구현

--- iOS/MiniVIBE/MiniVIBE/HomeFooterView.swift
+++ iOS/MiniVIBE/MiniVIBE/HomeFooterView.swift
@@ -8,30 +8,49 @@
 import SwiftUI
 
 struct HomeFooterView: View {
+    
     var body: some View {
         VStack {
-            Text("네이버(주) 사업자 정보 ⌵")
+            FooterText("네이버(주) 사업자 정보 ⌵")
             HStack {
-                Text("개인정보 처리방침")
-                Divider().frame(height: 15)
-                Text("오류신고 및 결제관련 문제해결")
+                FooterText("개인정보 처리방침")
+                FooterDivider()
+                FooterText("오류신고 및 결제관련 문제해결")
             }
             HStack {
-                Text("고객센터")
-                Divider().frame(height: 15)
-                Text("톡톡상담")
-                Divider().frame(height: 15)
-                Text("네이버 이용약관")
-                Divider().frame(height: 15)
-                Text("VIBE 이용약관")
+                FooterText("고객센터")
+                FooterDivider()
+                FooterText("톡톡상담")
+                FooterDivider()
+                FooterText("네이버 이용약관")
+                FooterDivider()
+                FooterText("VIBE 이용약관")
             }
-            Text("© NAVER Corp.")
         }
+    }
+}
+
+struct FooterText: View {
+    
+    var text: String
+    init(_ text: String) {
+        self.text = text
+    }
+    var body: some View {
+        Text(text).font(.caption).foregroundColor(.white)
+    }
+    
+}
+
+struct FooterDivider: View {
+    
+    var body: some View {
+        Divider().frame(height: 10).overlay(Color.white)
     }
 }
 
 struct HomeFooterView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeFooterView()
+        HomeFooterView().background(Color.black)
     }
 }
