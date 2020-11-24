0f5661beddffc06eed2ea2f21bab254e50da41b9
Park Ji Hong <hongzzi.dev@gmail.com>
2020-11-24 18:16:35 +0900

    [BE] Chore: init backend-setting

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import * as express from 'express';
+const app = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(3108, () => {
+  console.log('Example app listening on port 3108!');
+});
