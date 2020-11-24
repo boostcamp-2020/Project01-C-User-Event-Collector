e481fe2c73808e047b2f70bbbea06c0ba095d3cf
dochoi <dochoi@student.42seoul.kr>
2020-11-25 01:04:24 +0900

    [iOS] Feat: DarkMode Disable, Status Bar style 수정

---
+++ iOS/MiniVIBE/MiniVIBE/HomeView/HomeArtistSection/ArtistSection.swift
@@ -0,0 +1,20 @@
+//
+//  ArtistSection.swift
+//  MiniVIBE
+//
+//  Created by 최동규 on 2020/11/24.
+//
+
+import SwiftUI
+
+struct ArtistSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArtistSection_Previews: PreviewProvider {
+    static var previews: some View {
+        ArtistSection()
+    }
+}
