a9e2f5b63f06b72a317ef83fdb57e14993aacba9
이상경 <sknglee22@naver.com>
2020-11-25 12:26:07 +0900

    [Web] Chore: Eslint, Prettier Configuration 적용

--- frontend/pages/sample.tsx
+++ frontend/pages/sample.tsx
@@ -1,23 +1,21 @@
-import { GetStaticProps } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
-
-// import JJango from '../public/images/jjango.jpeg';
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
 
 interface allPostsData {
-    date: string;
-    title: string;
-    id: string;
-};
+  date: string;
+  title: string;
+  id: string;
+}
 
-export default function Home(props : allPostsData) {
-    return (
-        <div>hello it's next app
-            {/* <Image src={JJango} alt="me" width="64" height="64" /> */}
-            <Image src="../public/images/issuelogo.png" alt="me" width="64" height="64" />
-        </div>
-    )
+export default function Home(props: allPostsData): any {
+  return (
+    <div>
+      hello this is next app
+      <Image src="/images/faviconCat.png" alt="me" width="64" height="64" />
+    </div>
+  );
 }
 
 // export const getStaticProps: GetStaticProps = async () => {
