522db4b0f6cba04c69a996e9dbad5df9ad53491b
이상경 <sknglee22@naver.com>
2020-11-26 14:36:22 +0900

    [Web] Test: sample-rx.tsx 색상 변경 테스트

--- frontend/src/components/sample-rx.tsx
+++ frontend/src/components/sample-rx.tsx
@@ -30,7 +30,7 @@ const StyledP = styled.p`
     color: black;
     font-size: 5rem;
   `}
-  color: ${props => props.theme.color.pink};
+  color: ${props => props.theme.color.blue};
   font-size: 10rem;
 `;
 
