a82f4e966bc55461856d7d15b2d1b40b9919df92
Dokyun Kim <cs.dkkim@gmail.com>
2020-11-30 11:15:34 +0900

    [BE] Chore: 확장자 없는 import를 위한 lint 룰 추가
    Ex) './Track' (오류) => ./Track (허용)

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,14 @@ module.exports = {
     {
       files: ['**/*.ts', '**/*.tsx'],
       parser: '@typescript-eslint/parser',
-      settings: { react: { version: 'detect' } },
+      settings: {
+        react: { version: 'detect' },
+        'import/resolver': {
+          node: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          },
+        },
+      },
       env: {
         browser: true,
         node: true,
@@ -25,24 +32,35 @@ module.exports = {
         'plugin:react-hooks/recommended', // React hooks rules
         'plugin:jsx-a11y/recommended', // Accessibility rules
         'prettier/@typescript-eslint', // Prettier plugin
-        'plugin:prettier/recommended', // Prettier recommended rules 
+        'plugin:prettier/recommended', // Prettier recommended rules
       ],
       rules: {
-        "@typescript-eslint/no-use-before-define": "off",
-        "import/no-unresolved": "off",
-        "arrow-parens": ["warn", "as-needed"], // 화살표 함수의 파라미터가 하나일때 괄호 생략
-        "no-use-before-define": "off",
-        "no-unused-vars": ["off"],// 사용하지 않는 변수가 있을때 빌드에러가 나던 규칙 해제
-        "no-console": ["off"], // 콘솔을 쓰면 에러가 나던 규칙 해제
-        "react/jsx-props-no-spreading": ["warn"], // props spreading을 허용하지 않는 규칙 해제
+        '@typescript-eslint/no-use-before-define': 'off',
+        'import/no-unresolved': 'off',
+        'import/extensions': [
+          'error',
+          'ignorePackages',
+          {
+            js: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+          },
+        ],
+        'arrow-parens': ['warn', 'as-needed'], // 화살표 함수의 파라미터가 하나일때 괄호 생략
+        'no-use-before-define': 'off',
+        'no-unused-vars': ['off'], // 사용하지 않는 변수가 있을때 빌드에러가 나던 규칙 해제
+        'no-console': ['off'], // 콘솔을 쓰면 에러가 나던 규칙 해제
+        'react/jsx-props-no-spreading': ['warn'], // props spreading을 허용하지 않는 규칙 해제
         'no-underscore-dangle': ['off'], // camelCase를 따르는게 좋긴 하지만 `_`를 어쩔수 없이 써야하는 상황을 위해(ex 백엔드가 mongoDB)
-        'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
         'react/prop-types': 'off', // We will use TypeScript's types for component props instead
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off', // No need to import React when using Next.js
         'jsx-a11y/anchor-is-valid': 'off', // This rule is not compatible with Next.js's <Link /> components
         '@typescript-eslint/no-unused-vars': ['off'], // 사용하지 않는 변수가 있을때 빌드에러가 나던 규칙 해제
-        '@typescript-eslint/explicit-function-return-type': [ // I suggest this setting for requiring return types on functions only where usefull
+        '@typescript-eslint/explicit-function-return-type': [
+          // I suggest this setting for requiring return types on functions only where usefull
           'warn',
           {
             allowExpressions: true,
