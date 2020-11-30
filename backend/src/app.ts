92434301402c49a6a157a7581a7a6220edd15c1d
Dokyun Kim <cs.dkkim@gmail.com>
2020-11-30 10:54:53 +0900

    [BE] Fix: 서버 초기 구동 플로우 수정
    데이터베이스에 연결한 이후 Express가 작동하도록 수정하였습니다.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,6 @@ import * as morgan from 'morgan';
 import { createConnection } from 'typeorm';
 import * as dotenv from 'dotenv';
 import * as path from 'path';
-
 import apiRoute from './route';
 
 if (process.env.NODE_ENV === 'production') {
@@ -18,24 +17,25 @@ if (process.env.NODE_ENV === 'production') {
   throw new Error('process.env.NODE_ENV Not Found');
 }
 
-const app = express();
-app.set('port', process.env.PORT || 3108);
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(morgan('dev'));
-// TODO: 허용할 주소 정확히 명시하기
-app.use(cors());
-
-app.use('/api', apiRoute);
-
 // TODO: 사용자 정의 에러 핸들링 미들웨어
 createConnection()
   .then(() => {
     console.log('Database Connected :)');
+
+    const app = express();
+    app.set('port', process.env.PORT || 8000);
+
+    app.use(cookieParser());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(morgan('dev'));
+    // TODO: 허용할 주소 정확히 명시하기
+    app.use(cors());
+
+    app.use('/api', apiRoute);
+
+    app.listen(app.get('port'), () => {
+      console.log(`API Server App Listening on PORT ${app.get('port')}`);
+    });
   })
   .catch(error => console.log(error));
-
-app.listen(app.get('port'), () => {
-  console.log(`API Server App Listening on PORT ${app.get('port')}`);
-});
