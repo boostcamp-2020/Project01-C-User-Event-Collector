852e904ee741dc323621cd53b0a988fc8eb1d380
Dokyun Kim <cs.dkkim@gmail.com>
2020-11-30 12:20:51 +0900

    [BE] Fix: TypeORM config에 개발/배포 환경 분리
    개발 시에는 ts-node를 사용하여 src 디렉터리의 ts 스크립트를 사용하고
    배포 시에는 컴파일 결과인 dist 디렉터리의 js 스크립트를 사용합니다.

---
+++ backend/ormconfig.js
@@ -0,0 +1,39 @@
+const devConfig = {
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  synchronize: true,
+  logging: true,
+  entities: ['src/entities/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/migrations',
+    subscribersDir: 'src/subscriber',
+  },
+};
+
+const prodConfig = {
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  synchronize: true,
+  logging: false,
+  entities: ['dist/entities/**/*.js'],
+  migrations: ['dist/migration/**/*.js'],
+  subscribers: ['dist/subscriber/**/*.js'],
+  cli: {
+    entitiesDir: 'dist/entities',
+    migrationsDir: 'dist/migrations',
+    subscribersDir: 'dist/subscriber',
+  },
+};
+
+module.exports = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
