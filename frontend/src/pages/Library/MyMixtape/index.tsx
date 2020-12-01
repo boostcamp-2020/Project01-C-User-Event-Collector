079e002245be055fed6ea59533ed9108e0b2e6ec
Park Ji Hong <hongzzi.dev@gmail.com>
2020-12-01 16:13:23 +0900

    [FE] Feat: 보관함 template 작성
    type -> default, track 있음.
    track은 버튼있는 버전

---
+++ frontend/src/pages/Library/MyMixtape/index.tsx
@@ -0,0 +1,15 @@
+import styled from '@styles/themed-components';
+import Library from '@components/Template/Library/index.tsx';
+
+const MyTrack = () => (
+  <Library mainTitle={'믹스테잎'}>
+    <Container>
+      믹스테잎ㅍㅍㅍㅍ
+    </Container>
+  </Library>
+);
+
+const Container = styled.div`
+`;
+
+export default MyTrack;
