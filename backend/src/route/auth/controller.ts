f73d0972b41f55d7d24921d0195b1b441a10eda5
Dokyun Kim <cs.dkkim@gmail.com>
2020-12-01 15:21:58 +0900

    [BE] Feat: JWT 토큰 발급 미들웨어 추가

--- backend/src/route/auth/controller.ts
+++ backend/src/route/auth/controller.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+import User from '../../entities/User';
 
 const getToken = (req: Request, res: Response): void => {
-  console.log(req.user);
-  res.json({ hi: req.user });
+  const { id, nickname, email, profileURL } = req.user as User;
+  const token = jwt.sign({ id, nickname, email, profileURL }, process.env.JWT_SECRET as string, {
+    expiresIn: '1h',
+    noTimestamp: true,
+  });
+  res.json({ token });
 };
 
 export { getToken };
