2f0b429d84a10663db778625fd2acec452e6d538
sang-gyeong <sknglee22@naver.com>
2020-12-01 17:57:33 +0900

    [FE] Refactor: 예제 코드 삭제

--- frontend/pages/chart.tsx
+++ frontend/pages/chart.tsx
@@ -1,19 +1,27 @@
 import Sample from '@components/sample-rx';
 
-export default function Chart({ data }) {
-  console.log(data.data);
+export default function Chart() {
   return (
     <div>
-      <Sample text={data.data[5].artist} />
+      <Sample text="Hello! this is Chart page" />
     </div>
   );
 }
 
-export async function getStaticProps(context) {
-  const res = await fetch('http://localhost:5000/v1/naver/music/chart/100');
-  const data = await res.json();
+// export default function Chart({ data }) {
+//   console.log(data.data);
+//   return (
+//     <div>
+//       <Sample text={data.data[5].artist} />
+//     </div>
+//   );
+// }
 
-  return {
-    props: { data },
-  };
-}
+// export async function getStaticProps(context) {
+//   const res = await fetch('http://localhost:5000/v1/naver/music/chart/100');
+//   const data = await res.json();
+
+//   return {
+//     props: { data },
+//   };
+// }
