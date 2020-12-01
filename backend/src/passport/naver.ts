cdf67cdff4112879d3f9a17f0eb53b3be38b5f64
Dokyun Kim <cs.dkkim@gmail.com>
2020-12-01 14:17:25 +0900

    [BE] Refactor: passport-naver 전략 설정 모듈 분리
    추후 추가될 전략(passport-jwt)을 고려하여 분리합니다.

---
+++ backend/src/passport/naver.ts
@@ -0,0 +1,33 @@
+import * as passport from 'passport';
+import { Strategy as NaverStrategy } from 'passport-naver';
+import User from '../entities/User';
+
+const passportNaverConfig = (): void => {
+  const naverStrategyOptions = {
+    clientID: process.env.NAVER_CLIENT_ID as string,
+    clientSecret: process.env.NAVER_CLIENT_SECRET as string,
+    callbackURL: process.env.NAVER_CALLBACK_URL as string,
+  };
+
+  // Provider의 토큰은 사용하지 않음 (_ : accessToken __ : refreshToken)
+  const naverVerify = async (_: string, __: string, profile: any, done: any): Promise<void> => {
+    try {
+      const { email, nickname, profile_image: profileURL } = profile._json;
+      let user = await User.findOne({ email });
+      if (!user) {
+        user = new User();
+        user.email = email;
+        user.username = nickname;
+        user.nickname = nickname;
+        user.profileURL = profileURL;
+        await user.save();
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  };
+  passport.use('naver', new NaverStrategy(naverStrategyOptions, naverVerify));
+};
+
+export default passportNaverConfig;
