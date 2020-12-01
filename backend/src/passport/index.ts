4708accafed3f508625335018608e83f3d3c46b9
Dokyun Kim <cs.dkkim@gmail.com>
2020-12-01 15:23:08 +0900

    [BE] Feat: passport-jwt 설정 모듈 추가
    JWT 검증을 위해 패스포트 전략을 생성하고 이를 등록하는 모듈을
    생성하였습니다.

--- backend/src/passport/index.ts
+++ backend/src/passport/index.ts
@@ -1,7 +1,9 @@
 import passportNaverConfig from './naver';
+import passportJwtConfig from './jwt';
 
 const passportConfig = (): void => {
   passportNaverConfig();
+  passportJwtConfig();
 };
 
 export default passportConfig;
