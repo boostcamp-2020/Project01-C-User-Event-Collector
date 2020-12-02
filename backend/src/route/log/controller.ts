c63cc581bf9228ca8ca2bc3cd7e264c73033331c
sang-gyeong <sknglee22@naver.com>
2020-12-02 13:17:09 +0900

    [BE] Feat: log controller 함수 비동기로 변경

--- backend/src/route/log/controller.ts
+++ backend/src/route/log/controller.ts
@@ -1,9 +1,9 @@
 import * as express from 'express';
 import Log from '../../models/Log';
 
-const createLog = (req: express.Request, res: express.Response): void => {
+const createLog = async (req: express.Request, res: express.Response): Promise<void> => {
   const log = new Log(req.body);
-  log.save((err, logInfo) => {
+  await log.save((err, logInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
