27df7c43b5d2edfb8e9a26484a5e0a17473d82d9
sang-gyeong <sknglee22@naver.com>
2020-12-06 12:47:06 +0900

    [FE] Feat: EventHandler 함수 추가가

--- backend/src/route/auth/index.ts
+++ backend/src/route/auth/index.ts
@@ -1,12 +1,10 @@
 import * as express from 'express';
 import * as passport from 'passport';
-
 import { getToken } from './controller';
 
 const route = express.Router();
 
 route.get('/login', passport.authenticate('naver', { session: false }));
-
 route.get('/login/callback', passport.authenticate('naver', { session: false }), getToken);
 
 export default route;
