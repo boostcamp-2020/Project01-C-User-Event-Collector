46177a7f5ee0605c848040b21f2d48b7985eef4f
Dokyun Kim <cs.dkkim@gmail.com>
2020-12-11 23:29:06 +0900

    [BE] Feat: 로그인 여부에 상관 없이 로그 추가하는 기능 구현
    - 로그인 상태에서는 로그인 상태라고 표시하고 사용자의 고유 아이디를 로그에 담습니다.
    - 로그인 상태가 아니면 로그인 상태가 아니라고 표시합니다.

--- backend/src/route/log/controller.ts
+++ backend/src/route/log/controller.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
 import Log from '../../models/Log';
+import { IJwtPayload } from '../../passport/jwt';
 
 const createLog = async (req: Request, res: Response): Promise<void> => {
   const logInfo = req.body;
-  logInfo.userInfo = { isLoggedIn: true, user: req.user };
+  try {
+    const token = req.headers.authorization;
+    const user = jwt.verify(token as string, process.env.JWT_SECRET as string) as IJwtPayload;
+    logInfo.userInfo = { isLoggedIn: true, user: user.id };
+  } catch (err) {
+    logInfo.userInfo = { isLoggedIn: false };
+  }
   logInfo.userAgent = req.headers['user-agent'];
   const log = new Log(logInfo);
   console.log('@@@log : ', log);
