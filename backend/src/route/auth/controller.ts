bca6deae24737351de43ebb8a95a81104cde98fc
Dokyun Kim <cs.dkkim@gmail.com>
2020-12-11 21:49:26 +0900

    [BE] Feat: 토큰 만료 시간 제거

--- backend/src/route/auth/controller.ts
+++ backend/src/route/auth/controller.ts
@@ -5,7 +5,6 @@ import User from '../../entities/User';
 const getToken = (req: Request, res: Response): void => {
   const { id, nickname, email, profileURL } = req.user as User;
   const token = jwt.sign({ id, nickname, email, profileURL }, process.env.JWT_SECRET as string, {
-    expiresIn: '1h',
     noTimestamp: true,
   });
   if (req.headers['user-agent']?.includes('iPhone')) {
