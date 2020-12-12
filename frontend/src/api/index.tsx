21fd28bd1925328edbfde66661b9436e455c17d8
sang-gyeong <sknglee22@naver.com>
2020-12-12 15:44:47 +0900

    [WEB] Fix : 빌드 오류 수정

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mywibmlja25hbWUiOiLslYTroZzrpqwiLCJlbWFpbCI6InNrbmdsZWUyMkBuYXZlci5jb20iLCJwcm9maWxlVVJMIjoiaHR0cHM6Ly9waGluZi5wc3RhdGljLm5ldC9jb250YWN0LzIwMjAwNzA3XzEzNC8xNTk0MDkwNzM4MjIzRFV3d21fSlBFRy8yMDE2MDkxM18xNDMzMTcuanBnIn0.b8loSQaZF3aDjH2VxJGWDkAyj0vB2s4_Oi9dDuR1FqY';
 const api = axios.create({
   baseURL: 'http://localhost:8000/api/',
   headers: {
@@ -8,6 +10,7 @@ const api = axios.create({
     'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, PUT, OPTIONS',
     'Access-Control-Allow-Headers':
       'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+    Authorization: token,
   },
   validateStatus(status) {
     return status < 500;
