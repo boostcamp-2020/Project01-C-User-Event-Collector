cba47f3f242f44b0bf07640805b6eff3c8b97e30
Park Ji Hong <hongzzi.dev@gmail.com>
2020-12-12 11:20:18 +0900

    [FE] fix: build error

--- frontend/src/components/TrackList/index.tsx
+++ frontend/src/components/TrackList/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import styled from '@styles/themed-components';
 
 import TrackItem from '@components/Common/TrackItem';
