80863e65db86c0f64f6c84641c4d1eb9c2138604
sang-gyeong <sknglee22@naver.com>
2020-12-15 14:49:13 +0900

    [BE] Fix : 빌드오류 수정 및 iOS token 리다이렉트 url 변경

--- backend/src/route/auth/controller.ts
+++ backend/src/route/auth/controller.ts
@@ -5,7 +5,7 @@ import * as authService from '../../services/auth';
 const getToken = (req: Request, res: Response): void => {
   const token = authService.createToken(req.user as IJwtPayload);
   if (req.headers['user-agent']?.includes('iPhone')) {
-    return res.redirect(`minivibe://token?=${token}`); // 모바일
+    return res.redirect(`minivibe://token?${token}`); // 모바일
   }
   res.cookie('token', token, { maxAge: 1000 * 60 * 60 * 24 * 1, httpOnly: false });
   return res.redirect(process.env.SERVICE_URL as string); // 웹
