e95b475a6c0ade800b7c30eb59dec65141866956
sang-gyeong <sknglee22@naver.com>
2020-12-20 15:15:01 +0900

    [BE] Fix : iPad 분기 태우는 로직 수정

--- backend/src/route/auth/controller.ts
+++ backend/src/route/auth/controller.ts
@@ -5,8 +5,8 @@ import * as authService from '../../services/auth';
 const getToken = (req: Request, res: Response): void => {
   const token = authService.createToken(req.user as IJwtPayload);
   if (
-    req.headers['user-agent']?.includes('iPhone') ||
-    req.headers['user-agent']?.includes('iPad')
+    req.headers['user-agent']?.includes('MiniVIBE') ||
+    req.headers['user-agent']?.includes('MiniVIBE/1')
   ) {
     return res.redirect(`minivibe://token?${token}`); // 모바일
   }
