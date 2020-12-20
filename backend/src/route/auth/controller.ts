41530b168c8ff721128cc63cc23000b8ec8d7726
sang-gyeong <sknglee22@naver.com>
2020-12-20 15:03:00 +0900

    [WEB] BE : getToken iPad 분기처리 로직 추가

--- backend/src/route/auth/controller.ts
+++ backend/src/route/auth/controller.ts
@@ -4,7 +4,10 @@ import * as authService from '../../services/auth';
 
 const getToken = (req: Request, res: Response): void => {
   const token = authService.createToken(req.user as IJwtPayload);
-  if (req.headers['user-agent']?.includes('iPhone')) {
+  if (
+    req.headers['user-agent']?.includes('iPhone') ||
+    req.headers['user-agent']?.includes('iPad')
+  ) {
     return res.redirect(`minivibe://token?${token}`); // 모바일
   }
   res.cookie('token', token, { maxAge: 1000 * 60 * 60 * 24 * 1, httpOnly: false });
